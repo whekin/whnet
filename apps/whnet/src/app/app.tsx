6e858c7bcecf9b073638342f68c80bf21a794ba0
whekin <stanislavkalishin@gmail.com>
2022-01-05 01:21:13 +0300

    Implement basic login component
    feature: validation
    install mui/material

--- apps/whnet/src/app/app.tsx
+++ apps/whnet/src/app/app.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import fetch from 'cross-fetch';
 
 import { Login } from '@whnet/ui';
+import Box from '@mui/material/Box';
+
+const httpLink = createHttpLink({
+  fetch,
+  uri: process.env['NX_GRAPHQL_ENDPOINT_URI'],
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6IndoZWtpbiIsImlhdCI6MTY0MTMyNDEzNCwiZXhwIjoxNjQxNTgzMzM0fQ._eZH7NSBsosjG7oF6Gx2kWKOAyJSC1unkBARRN0gyoU';
+  // localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: createHttpLink({
-    fetch,
-    // eslint-disable-next-line @typescript-eslint/dot-notation
-    uri: process.env['NX_GRAPHQL_ENDPOINT_URI'],
-  }),
+  link: authLink.concat(httpLink),
 });
 
 const App = () => (
   <ApolloProvider client={client}>
     <h1>Whnet</h1>
-    <Login />
+    <Box sx={{ width: 300 }}>
+      <Login />
+    </Box>
   </ApolloProvider>
 );
 
--- libs/ui/src/lib/login/login.tsx
+++ libs/ui/src/lib/login/login.tsx
@@ -1,31 +1,65 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import { Box, TextField, Stack, Button } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
-import { useUserQuery } from '@whnet/data-access';
+const validationSchema = Yup.object().shape({
+  nickname: Yup.string()
+    .required('Nickname is required')
+    .min(3, 'Nickname must be at least 3 characters')
+    .max(20, 'Nickname must not exceed 20 characters')
+    .matches(
+      /^[a-z_]+$/,
+      'Nickname must consist of lowercase latin characters and underscores'
+    ),
+  password: Yup.string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters')
+    .max(50, 'Password must not exceed 50 characters'),
+});
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface LoginProps {}
 
-const StyledLogin = styled.div`
-  color: green;
-`;
-
 export const Login = (props: LoginProps) => {
-  const { loading, error, data } = useUserQuery();
+  const onSubmit = (data: any) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(validationSchema) });
 
   return (
-    <StyledLogin>
-      <ul>
-        {data?.users.map((user) => (
-          <li key={user.id}>
-            {user.nickname}: {user.id}
-          </li>
-        ))}
-      </ul>
-    </StyledLogin>
+    <Box component="form" noValidate autoComplete="off">
+      <Stack spacing={2}>
+        <TextField
+          type="text"
+          label="nickname"
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...register('nickname')}
+          error={!!errors['nickname']}
+          helperText={errors['nickname']?.message}
+          required
+        />
+        <TextField
+          type="password"
+          label="password"
+          autoComplete="current-password"
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...register('password')}
+          error={!!errors['password']}
+          helperText={errors['password']?.message}
+          required
+        />
+        <Button variant="contained" onClick={handleSubmit(onSubmit)}>
+          Login or sign up
+        </Button>
+      </Stack>
+    </Box>
   );
 };
 
